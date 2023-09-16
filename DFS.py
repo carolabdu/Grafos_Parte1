9a1724c955a229d5ea335ea247641c41e64a7e8f
carolabdu <108949383+carolabdu@users.noreply.github.com>
2023-09-15 23:22:48 -0300

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,1 +1,19 @@
 
+class DFS(Graph,Stack):
+    def __init__(self, v, a, vi):
+        super().__init__(v, a)
+        self.vi= vi #vétice que começaremos a busca
+        self.marcados = []
+        self.s = Stack()
+        self.s.push(self.vi)
+
+    def algoritmo():
+        while self.s != []:
+            u = self.s.peek() #pega vértice no topo da lista 
+            self.s.pop()
+            if u not in self.marcados: # (implemetar para ver se tá na lista): 
+                self.marcados += [u]
+                #Para cada aresta (u,v) incidente a u
+                #Adicionar v em P // no topo
+
+DFS(nodes,edges).motra_v()
