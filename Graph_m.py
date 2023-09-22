87bae12c986bfd01a8c032c22df9dbed7f848e4c
carolabdu <108949383+carolabdu@users.noreply.github.com>
2023-09-21 22:51:47 -0300

    Update Graph_m.py

--- Graph_m.py
+++ Graph_m.py
@@ -40,3 +40,29 @@ class Graph_m: #grafo em matriz
         soma_graus = np.sum(np.sum(self.matriz, axis=0), axis=0)
         grau_med = (soma_graus/self.v)
         print(grau_med)
+
+
+    def DFS(self,vi):
+        self.marcados = [] #inicia vetor de marcação
+        self.s = Stack() #cria pilha vazia
+        self.s.push(vi)  #adiciona a raiz na fila
+        self.pai = np.zeros([self.v]) #inicia vetor com os pais
+        self.nivel = np.zeros([self.v]) #inicia vetor dos níveis
+        self.pai[vi-1] = None #indica que não tem pai pois é raiz
+        self.nivel[vi -1]= 0 #nivel da raiz é zero 
+        while self.s.isEmpty()==False : #enquanto pilha não estiver vazia 
+            u = self.s.peek() #pega vértice no topo da lista
+            self.s.pop() #remover u da pilha 
+            if u not in self.marcados: # (implemetar para ver se tá na lista): 
+                self.marcados += [u] #adicionar vétice aos marcados
+                for k in range(self.v): #para cada vizinho de u 
+                    vizinho = self.matriz[u-1][k] #verificar se elemento da matriz é 1
+                    if vizinho ==1: #ou seja se é vizinho de fato
+                        self.s.push(k+1)  #botar na fila 
+                        if k+1 not in self.marcados:  #se k ainda não foi marcado
+                            self.pai[k] = u   #escrevre que u descobriu (vai mudando ao longo do algoritmo)
+                            self.nivel[k] = self.nivel[u-1] + 1  #verifica o nível do pai de k e soma 1
+        print('marcados:',self.marcados)
+        print('pais:', self.pai)
+        print('niveis:', self.nivel)    
+    
