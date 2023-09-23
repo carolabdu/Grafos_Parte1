d62d9359b471af2a0d0142364faae4f708033f2c
carolabdu <108949383+carolabdu@users.noreply.github.com>
2023-09-23 15:46:29 -0300

    Update Graph_l.py
    diameter

--- Graph_l.py
+++ Graph_l.py
@@ -103,4 +103,12 @@ class Graph_l:
         distancia = self.BFStree[1][v2-1]
         print(distancia)
 
+    def diameter(self):  
+        diameter = 0
+        for i in range(int(self.v)):
+            self.BFS(i)
+            if self.maxlevel > diameter:
+                diameter = self.maxlevel
+        print(diameter)
+
  
