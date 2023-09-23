5364c1d0a78ea727d43fa22754d4800094c94491
carolabdu <108949383+carolabdu@users.noreply.github.com>
2023-09-23 14:58:28 -0300

    Update Graph_m.py

--- Graph_m.py
+++ Graph_m.py
@@ -48,37 +48,36 @@ class Graph_m: #grafo em matriz
 
 
     def DFS(self,vi):
-        self.marcados = [] #inicia vetor de marcação
+        self.marcados = np.zeros(self.v,dtype=int) #inicia vetor de marcação
         self.s = Stack() #cria pilha vazia
         self.s.push(vi)  #adiciona a raiz na fila
-        self.pai = np.zeros([self.v]) #inicia vetor com os pais
-        self.nivel = np.zeros([self.v]) #inicia vetor dos níveis
-        self.pai[vi-1] = None #indica que não tem pai pois é raiz
-        self.nivel[vi -1]= 0 #nivel da raiz é zero 
+        pai = np.zeros([self.v], dtype=int) #inicia vetor com os pais
+        nivel = np.zeros([self.v],dtype=int) #inicia vetor dos níveis
+        pai[vi-1] = 0 #indica que não tem pai pois é raiz
+        nivel[vi -1]= 0 #nivel da raiz é zero 
         while self.s.isEmpty()==False : #enquanto pilha não estiver vazia 
             u = self.s.peek() #pega vértice no topo da lista
             self.s.pop() #remover u da pilha 
-            if u not in self.marcados: # (implemetar para ver se tá na lista): 
-                self.marcados += [u] #adicionar vétice aos marcados
+            if self.marcados[u-1]==0: # (implemetar para ver se tá na lista): 
+                self.marcados[u-1]=1 #adicionar vétice aos marcados
                 for k in range(self.v): #para cada vizinho de u 
                     vizinho = self.matriz[u-1][k] #verificar se elemento da matriz é 1
                     if vizinho ==1: #ou seja se é vizinho de fato
                         self.s.push(k+1)  #botar na fila 
-                        if k+1 not in self.marcados:  #se k ainda não foi marcado
-                            self.pai[k] = u   #escrevre que u descobriu (vai mudando ao longo do algoritmo)
-                            self.nivel[k] = self.nivel[u-1] + 1  #verifica o nível do pai de k e soma 1
+                        if self.marcados[k]==0:  #se k ainda não foi marcado
+                            pai[k] = u   #escrevre que u descobriu (vai mudando ao longo do algoritmo)
+                            nivel[k] = nivel[u-1] + 1  #verifica o nível do pai de k e soma 1
         print('marcados:',self.marcados)
-        print('pais:', self.pai)
-        print('niveis:', self.nivel)    
+        print('pais:', pai)
+        print('niveis:', nivel)   
 
     def BFS(self,vi):
-        self.marked = [] #list with the nodes that are being or has already been explored
+        self.marked = np.zeros(self.v,dtype=int) #list with the nodes that are being or has already been explored
         self.Q = Queue() #Creates an empty Queue
         pai = np.zeros([self.v], dtype=int) #inicia vetor com os pais
         nivel = np.zeros([self.v],dtype=int) #inicia vetor dos níveis
         pai[vi-1] = 0 #indica que não tem pai pois é raiz
-    
-        self.marked.append(vi) #Marks the initial node(where we start the search)
+        self.marked[vi-1]=1#Marks the initial node(where we start the search)
         self.Q.add(vi) #Adds the firts node in the FIRTS POSITION of the Queue
         pai[vi-1] = 0 #indica que é raiz 
         while self.Q.is_empty() == False: #The search continues until the Queue is empty, when all the nodes have been explored
@@ -86,14 +85,14 @@ class Graph_m: #grafo em matriz
             for w in range(self.v): #para cada vizinho de v
                     vizinho = self.matriz[v-1][w] #verificar se elemento da matriz é 1
                     if vizinho ==1: #ou seja se é vizinho de fato #Explores all of v's neighbors and mark the ones who hasn't been explores
-                        if w+1 not in self.marked: 
+                        if self.marked[w]==0: 
                             pai[w]= v #if w is not marked, it means the node that discovered it was v
                             nivel[w] = nivel[v-1]+1 #hanges the level of w
-                            self.marked.append(w+1) #Marks w if it's not been discovered yet
+                            self.marked[w]=1 #Marks w if it's not been discovered yet
                             self.Q.add(w+1) #Adds w to the first position of the Queue
 
         self.maxlevel = np.argmax(nivel)
-        
+
         print('marcados:',self.marked)
         print('pais:', pai)
         print('niveis:', nivel) 
