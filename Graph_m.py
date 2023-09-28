f38a5bf79461d2cb341c0802554f13ff1f16151d
carolabdu <108949383+carolabdu@users.noreply.github.com>
2023-09-28 18:14:20 -0300

    Update Graph_m.py

--- Graph_m.py
+++ Graph_m.py
@@ -10,44 +10,56 @@ class Graph_m: #grafo em matriz
             self.matriz[v1-1][v2-1] = 1 #se os vértices estão conecatados 
             self.matriz[v2-1][v1-1]=1 #grafo não é direcionado, 'espelhamos' o resultado
 
-    def vertices(self):
+    def vertices(self,p):
+        if p==1:
+            print (self.v)
         return self.v
 
-    def aresta(self):
+    def aresta(self,p):
+        if p==1:
+            print (a)
         return len(self.a)
     
     def mostra_matriz(self):
         for i in range(self.v):
             print(self.matriz[i])    
     
-    def grau (self):
-        graus = np.sum(self.matriz, axis=0)
-        print(graus)
+    #def grau (self,p):
+        #graus = np.sum(self.matriz, axis=0)
+        #print(graus)
 
-    def graumin(self):
+    def graumin(self,p):
         graus = np.sum(self.matriz, axis=0)
         grau_min = np.argmin(graus)
-        print(grau_min)
+        if p==1:
+            print(grau_min)
+        return grau_min
 
-    def graumax(self):
+    def graumax(self, p):
         graus = np.sum(self.matriz, axis=0)
         grau_max = np.argmax(graus)
-        print(grau_max)
+        if p==1:
+          print(grau_max)
+        return grau_max
         
     
-    def graumed(self):
+    def graumed(self, p):
         graus = np.sum(self.matriz, axis=0)
         soma_graus = np.sum(np.sum(self.matriz, axis=0), axis=0)
         grau_med = (soma_graus/self.v)
-        print(grau_med)
+        if p==1:
+            print(grau_med)
+        return grau_med
 
-    def mediano(self):
+    def mediano(self,p):
         graus = np.sum(self.matriz, axis= 0)
         mediano = np.median(graus)
+        if p==1:
+            print(int(mediano)
         return int(mediano)
 
 
-    def DFS(self,vi):
+    def DFS(self,vi,p):
         self.marcados = np.zeros(self.v,dtype=int) #inicia vetor de marcação
         self.s = Stack() #cria pilha vazia
         self.s.push(vi)  #adiciona a raiz na pilha
@@ -68,9 +80,11 @@ class Graph_m: #grafo em matriz
                             pai[k] = u   #escrevre que u descobriu (vai mudando ao longo do algoritmo)
                             nivel[k] = nivel[u-1] + 1  #verifica o nível do pai de k e soma 1
         self.DFStree = [pai, nivel]
+        if p==1: 
+            print('pais':, pai, 'níveis:', nivel)
         return (self.DFStree)
 
-    def BFS(self,vi):
+    def BFS(self,vi,p):
         self.marked = np.zeros(self.v,dtype=int) #list with the nodes that are being or has already been explored
         self.Q = Queue() #Creates an empty Queue
         pai = np.array([-1] * self._vertices, dtype = int) #inicia vetor com os pais
@@ -91,36 +105,32 @@ class Graph_m: #grafo em matriz
                             self.Q.add(w+1) #Adds w to the first position of the Queue
 
         self.maxlevel = np.argmax(nivel)
-
         self.BFStree = [pai, nivel,self.maxlevel]
+        if p==1: 
+            print('pais':, pai, 'níveis:', nivel)
         return (self.BFStree)
 
-    def mostra_DFS(self,vi):
-        self.DFS(vi)
-        print (self.DFStree)
-
-    def mostra_BFS(self,vi):
-        self.BFS(vi)
-        print (self.BFStree)
-
-    def distancia(self, v1, v2):
+    def distancia(self, v1, v2,p):
         self.BFS(v1)
         if self.BFStree[0][v2 -1] == -1: 
             distancia = infinita #ou seja vértices não estão conectadas
         else: 
             distancia = self.BFStree[1][v2-1]
-        print(distancia)
+        if p==1:
+            print(distancia)
+        return distancia  
 
-    def diameter(self):  
+    def diameter(self, p):  
         diameter = 0
         for i in range(int(self.v)):
             self.BFS(i)
             if self.maxlevel > diameter:
                 diameter = self.maxlevel
-        print(diameter)
+        if p==1:
+            print(diameter)
+        return diameter
 
-    
-    def cc(self):  #não testada e fazer uma função para ordenar 
+    def cc(self,p):  #não testada e fazer uma função para ordenar 
         cc = []
         vistos = np.zeros(self.v,dtype=int)
         for vi in range(1,self.v +1):
@@ -133,6 +143,8 @@ class Graph_m: #grafo em matriz
                         marcados[0].append(k+1) #índice é uma unidade menor que o vétice
                         marcados[1] += 1
             cc.append(marcados)
+        if p==1:
+            print(cc)
         return cc  
 
     
