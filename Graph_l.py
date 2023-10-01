1b123467d0212473f93c8ba6f73f76dd6127aeb7
Alice Duarte F Ribeiro <108374759+alicedfr@users.noreply.github.com>
2023-10-01 13:20:16 -0300

    Update Graph_l.py

--- Graph_l.py
+++ Graph_l.py
@@ -1,52 +1,51 @@
-class Graph_l:
-    def __init__(self, v, a):
+class Graph_l: #Classe de grafos seguindo a representação de lista de adjacência
+    def __init__(self, v, a): #(número de vértices, número de arestas)
         self.v = v
         self.a = np.array(a)
-        self.num_arestas = len(self.a)
-        self.lista = [[] for i in range(self.v)] #não sabemos quantos elementos tem em cada linha
-        g = np.zeros(self.v,dtype=int)
+        self.num_arestas = len(self.a) 
+        self.lista = [[] for i in range(self.v)] #Não sabemos quantos elementos tem em cada linha (quantos vizinhos cada vértice tem)
+        g = np.zeros(self.v,dtype=int) #Vetor de zeros, onde vamos guardar o grau de cada vértice
         for i in range(self.num_arestas):
-            u=self.a[i][0]
-            v= self.a[i][1]
-            self.lista[u-1].append(v) #adicionar vértice v na linha u
-            self.lista[v-1].append(u)
-            g [u-1] += 1  #tualiza os graus 
+            u=self.a[i][0] #Pega o primeiro vértice do par de arestas
+            v= self.a[i][1] #Pega o segundo vértice do par de arestas
+            self.lista[u-1].append(v) #Adiciona o vértice v na linha u (o índice é u-1, pois i se inicia no 0)
+            self.lista[v-1].append(u) #Adiciona o vértice u na linha v
+            g [u-1] += 1  #Atualiza os graus 
             g [v-1] += 1
         self.graus = g
-        #self.lista = np.asarray(self.lista, dtype=list)
-        
-    def vertices(self,p):
+
+    #Algumas funções possuem o padrão "p", que define se o resultado dele ser escrito no arquivo de saída ou não
+    def vertices(self,p): #Retorna o número de vértices do grafo 
         if p ==1:
-            print(self.v)
-        return(self.v)
+            arquivo_saida.write(f'Número de vértices: {self.v}') ######PAREI AQUI
+        return self.v 
     
-    def arestas(self,p):
-        a = int(np.sum(self.graus))/2
+    def arestas(self,p): #Retorna o número de arestas do grafo
+        a = int(np.sum(self.graus))/2 #Usamos a relação: Grau = 2*#Arestas
         if p==1:
-            print(a)
+            arquivo.write(f'Número de arestas: {a}')
         return a
 
-    def mostra_lista(self):
+    def mostra_lista(self): #Retorna a lista de adjacência
         return (self.lista) 
     
-    def grau(self):
-        print (self.graus)
+    def mostra_grau(self): #Retorna a lista de graus
+        return (self.graus)
 
-    def graumin(self,p):
+    def graumin(self,p=0): #Retorna o grau mínimo
         grau_min = np.min(self.graus)
         if p==1:
             print(grau_min)
         return grau_min
 
-    def graumax(self,p):
+    def graumax(self,p): #Retorna o grau máximo
         grau_max = np.max(self.graus)
         if p==1:
             print(grau_max)
         return grau_max
         
-    def graumed(self,p):
-        soma_graus = np.sum(self.graus)
-        grau_med = (soma_graus/self.v)
+    def graumed(self,p): #Retorna a média dos graus
+        grau_med = np.mean(self.graus)
         if p==1:
             print(grau_med)
         return grau_med
@@ -55,10 +54,10 @@ class Graph_l:
         mediano = np.median(self.graus)
         if p==1: 
             print (int(mediano))
-        return (int(mediano))
+        return int(mediano)
 
 
-    def DFS(self,vi,p):
+    def DFS(self,vi,p=0): #Implementação da busca em profundidade
         self.marcados = np.zeros(self.v,dtype=int) #inicia vetor de marcação
         self.s = Stack() #cria pilha vazia
         self.s.push(vi)  #adiciona a raiz na fila
@@ -74,7 +73,7 @@ class Graph_l:
                 for k in self.lista[u-1]: #para cada vizinho de u 
                     self.s.push(k)  #botar na fila 
                     if self.marcados[k-1]==0:  #se k ainda não foi marcado
-                        pai[k-1] = u   #escrevre que u descobriu (vai mudando ao longo do algoritmo)
+                        pai[k-1] = u   #escreve que u descobriu (vai mudando ao longo do algoritmo)
                         nivel[k-1] = nivel[u-1] + 1  #verifica o nível do pai de k e soma 1
         
         self.DFStree = [pai, nivel]
@@ -82,66 +81,62 @@ class Graph_l:
             print('pais:', pai, 'níveis', nivel)
         return (self.DFStree)  
 
-    def BFS(self,vi,p):
-        self.marked = np.zeros(self.v,dtype=int) #list with the nodes that are being or has already been explored
-        self.Q = Queue() #Creates an empty Queue
+    def BFS(self,vi,p): #Implementação da busca em largura
+        self.marcados = np.zeros(self.v,dtype=int) #Lista com os nós que já foram explorados
+        self.Q = Queue() #Cria uma fila vazia
         pai = np.array([-1] * self.v, dtype = int) #inicia vetor com os pais
         nivel = np.array([-1] * self.v, dtype = int) #inicia vetor dos níveis
-        pai[vi-1] = 0 #indica que não tem pai pois é raiz
-        nivel[vi -1] =0
-        self.marked[vi-1]=1#Marks the initial node(where we start the search)
-        self.Q.add(vi) #Adds the firts node in the FIRTS POSITION of the Queue
-        pai[vi-1] = 0 #indica que é raiz 
-        while self.Q.is_empty() == False: #The search continues until the Queue is empty, when all the nodes have been explored
-            v = self.Q.pop() #Removes the last node of the Queue, which is the older one and atributes to variable v
-            for w in self.lista[v-1]: #para cada vizinho de v
-                if self.marked[w-1]==0: 
-                    pai[w-1]= v #if w is not marked, it means the node that discovered it was v
-                    nivel[w-1] = nivel[v-1]+1 #hanges the level of w
-                    self.marked[w-1]=1 #Marks w if it's not been discovered yet
-                    self.Q.add(w) #Adds w to the first position of the Queue
+        nivel[vi -1] = 0
+        self.marcados[vi-1]=1#Marca vi, que é o nó em que começamos a busca
+        self.Q.add(vi) #Adciona o nó na fila
+        pai[vi-1] = 0 #Indica que é raiz 
+        while self.Q.is_empty() == False: #A busca continua até a lista ficar vazia(Até todos os vértices serem explorados)
+            v = self.Q.pop() #Remove o último vértice da fila, que corresponde ao vértice mais antigo
+            for w in self.lista[v-1]: #Visita os vizinhos de v
+                if self.marcados[w-1]==0: 
+                    pai[w-1]= v #Se w não tiver sido marcado, o vértice que o descobriu(seu pai) foi v
+                    nivel[w-1] = nivel[v-1]+1 #Muda o nível atual para o nível de w
+                    self.marked[w-1]=1 #Marca w se não foi descoberto ainda
+                    self.Q.add(w) #Adiciona w na primeira poição da fila
 
-        self.maxlevel = np.max(nivel)
+        self.maxlevel = np.max(nivel) #Pega o nível máximo
 
-        self.BFStree = [pai, nivel,self.maxlevel]
+        self.BFStree = [pai, nivel, self.maxlevel] 
         if p==1:
             print('pais:', pai, 'níveis', nivel)
         return (self.BFStree)
 
-    def distancia(self, v1, v2,p):
-        self.BFS(v1,0)
-        if self.BFStree[0][v2 -1] == -1: 
-            distancia = 'infinita' #ou seja vértices não estão conectadas
+    def distancia(self, v1, v2,p=0): #Retorna a distâncoa entre dois vértices
+        self.BFS(v1) #Roda a BFS para o primeiro vértice
+        if self.BFStree[0][v2 -1] == -1: #Checa se o segundo vértice está conectado ao primeiro(caso ele tenha um pai na BFStree, então os dois são conectados)
+            distancia = 'infinita' #Ou seja vértices não estão conectadas
         else: 
-            distancia = self.BFStree[1][v2-1]
+            distancia = self.BFStree[1][v2-1] #O nível será a distância entre os vértices
         if p==1:
             print(distancia)
         return distancia
 
-    def diameter(self,p):  
-        if self.v < 1000:
+    def diameter(self,p=0):  #Retorna o diãmetro do grafo
+        if self.v < 1000: #Para otimizar o código, escolhemos 1000 vértices aleatórios de cada grafo para aproximar o diâmetro
             diameter = 0
             for i in range(1,self.v):
-                self.BFS(i,0)
-                print(self.maxlevel)
-                if self.maxlevel > diameter:
-                    diameter = self.maxlevel
-            if p==1:
-                print(diameter)
-            return diameter
+                self.BFS(i) #Roda a BFS para cada vértice
+                #print(self.maxlevel)
+                if self.maxlevel > diameter: #Procura o maior nível dentre os vértices, que será o diâmetro
+                    diameter = self.maxlevel 
         else: 
             for i in range(1000):
                 vi = random.randint(1, self.v)
                 diameter = 0
-                self.BFS(vi,0)
-                if self.maxlevel > diameter:
+                self.BFS(vi) #Roda a BFS para 1000 vértices aleatórios
+                if self.maxlevel > diameter: #Procura o maior nível dentre os vértices, que será o diâmetro
                     diameter = self.maxlevel
-            if p==1:
-                print(diameter)
-            return diameter
+        if p==1:
+            print(diameter)
+        return diameter
 
     
-    def cc(self,p):  #não testada e fazer uma função para ordenar 
+    def cc(self,p):  #Retorna o número de componentes conexas,
         cc = []
         vistos = np.zeros(self.v,dtype=int)
         for vi in range(1,self.v +1):
