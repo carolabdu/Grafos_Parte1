ad4f462ba406da3e8542f0731cf1351e0fd5a231
Alice Duarte F Ribeiro <108374759+alicedfr@users.noreply.github.com>
2023-10-01 11:49:38 -0300

    Update Abrir arquivo.py
    Mudança do nome de variáveis e comentando o código

--- Abrir arquivo.py
+++ Abrir arquivo.py
@@ -1,8 +1,8 @@
 
-with open('grafo_n.txt', 'r') as archive:
-        text = [int(node) for node in archive.read().split() if node.isdigit()]
-        nodes = text[0]
-        edges = []
-        for i in range(len(text[1:])//2):
-            edges += [text[2*i+1:2*i+3]]
-print (edges)
+with open('grafo_n.txt', 'r') as arquivo:
+        texto = [int(vertice) for vertice in arquivo.read().split() if vertice.isdigit()] #Cria uma lista com o conteúdo presente no arquivo de teste
+        vertices = texto[0] #Pega o primeiro elemento da lista acima, que correnspode ao número de vértices do grafo
+        arestas = [] #Lista vazia que vai conter todos os pares de arestas do grafo
+        for i in range(len(texto[1:])//2):
+            arestas += [texto[2*i+1:2*i+3]] #Atualiza a lista de arestas, criando sublistas com os pares de arestas
+#print (arestas)
