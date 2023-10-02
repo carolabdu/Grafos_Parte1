9ae5631763427901ae569239be48de9fa1352a25
Alice Duarte F Ribeiro <108374759+alicedfr@users.noreply.github.com>
2023-10-02 05:28:11 -0300

    Update Graph_m.py

--- Graph_m.py
+++ Graph_m.py
@@ -1,7 +1,7 @@
 import numpy as np 
 class Graph_m: #grafo em matriz 
-    def __init__(self, arq, v, a):
-        self.arq = arq
+    def __init__(self, v, a):
+        self.arquivo_saida = []
         self.v = v
         self.a = a   #mudar forma de como guardar uasando numpy
         self.matriz = np.zeros([self.v, self.v], dtype=int)
@@ -12,15 +12,13 @@ class Graph_m: #grafo em matriz
             self.matriz[v2-1][v1-1]=1 #grafo não é direcionado, 'espelhamos' o resultado
 
     def vertices(self,p):
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         if p==1:
-             arquivo_saida.write(f'\nNúmero de vértices: {self.v}')
+             self.arquivo_saida.append(f'\nNúmero de vértices: {self.v}')
         return self.v
 
     def arestas(self,p):
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         if p==1:
-            arquivo_saida.write(f'\nNúmero de arestas: {int(len(self.a)}')
+            self.arquivo_saida.append(f'\nNúmero de arestas: {int(len(self.a)}')
         return len(self.a)
     
     def mostra_matriz(self):
@@ -32,42 +30,37 @@ class Graph_m: #grafo em matriz
         #print(graus)
 
     def graumin(self,p):
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         graus = np.sum(self.matriz, axis=0)
         grau_min = np.min(graus)
         if p==1:
-            arquivo_saida.write(f'\nGrau mínimo: {grau_min}')
+            self.arquivo_saida.append(f'\nGrau mínimo: {grau_min}')
         return grau_min
 
     def graumax(self, p):
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         graus = np.sum(self.matriz, axis=0)
         grau_max = np.max(graus)
         if p==1:
-          arquivo_saida.write(f'\nGrau máximo: {grau_max}')
+          self.arquivo_saida.append(f'\nGrau máximo: {grau_max}')
         return grau_max
         
     
     def graumed(self, p):
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         graus = np.sum(self.matriz, axis=0)
         soma_graus = np.sum(np.sum(self.matriz, axis=0), axis=0)
         grau_med = (soma_graus/self.v)
         if p==1:
-            arquivo_saida.write(f'\nGrau médio: {grau_med}')
+            self.arquivo_saida.append(f'\nGrau médio: {grau_med}')
         return grau_med
 
     def mediano(self,p):
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         graus = np.sum(self.matriz, axis= 0)
         mediano = np.median(graus)
         if p==1:
-            arquivo_saida.write(f'\nMediana dos graus: {mediano}')
+            self.arquivo_saida.append(f'\nMediana dos graus: {mediano}')
         return int(mediano)
 
 
     def DFS(self,vi,p):
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         self.marcados = np.zeros(self.v,dtype=int) #inicia vetor de marcação
         self.s = Stack() #cria pilha vazia
         self.s.push(vi)  #adiciona a raiz na pilha
@@ -89,11 +82,10 @@ class Graph_m: #grafo em matriz
                             nivel[k] = nivel[u-1] + 1  #verifica o nível do pai de k e soma 1
         self.DFStree = [pai, nivel]
         if p==1: 
-           arquivo_saida.write(f'\nBusca DFS:\nPais: {pai}\nNíveis: {nivel}')
+           self.arquivo_saida.append(f'\nBusca DFS:\nPais: {pai}\nNíveis: {nivel}')
         return (self.DFStree)
 
     def BFS(self,vi,p):
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         self.marcados = np.zeros(self.v,dtype=int) #Lista com os vétices explorados
         self.Q = Queue() #Cria uma fila vazia
         pai = np.array([-1] *self.v, dtype = int) #Inicia vetor com os pais
@@ -117,22 +109,20 @@ class Graph_m: #grafo em matriz
         self.maxlevel = np.max(nivel) #Pega o maior nível
         self.BFStree = [pai, nivel,self.maxlevel]
         if p==1: 
-            arquivo_saida.write(f'\nBusca BFS:\nPais: {pai}\nNíveis: {nivel}')
+            self.arquivo_saida.append(f'\nBusca BFS:\nPais: {pai}\nNíveis: {nivel}')
         return (self.BFStree)
 
     def distancia(self, v1, v2,p): #Retorna a distância entre dois vértices
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         self.BFS(v1,0) #Roda a BFS para v1
         if self.BFStree[0][v2 -1] == -1: #Se o pai de v2 for -1, ele não está conectado com v1
             distancia = 'infinita' #ou seja vértices não estão conectadas
         else: 
             distancia = self.BFStree[1][v2-1] #O nível de v2 será a distância dele até v1
         if p==1:
-            arquivo_saida.write(f'\nDistância entre {v1} e {v2}: {distancia}')
+            self.arquivo_saida.append(f'\nDistância entre {v1} e {v2}: {distancia}')
         return distancia  
 
     def diameter(self, p): #Retorna o diâmetro do grafo
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         if self.v < 1000: 
             diameter = 0
             for i in range(1,self.v):
@@ -147,11 +137,10 @@ class Graph_m: #grafo em matriz
                 if self.maxlevel > diameter:
                     diameter = self.maxlevel
         if p==1:
-        arquivo_saida.write(f'\nDiâmetro do grafo: {diameter}')
+            self.arquivo_saida.append(f'\nDiâmetro do grafo: {diameter}')
         return diameter
 
     def cc(self,p):  #Retorna a quantidade de componentes conexas, a quantidade de nós da maior delas e da menor
-        arquivo_saida = open(f'Resultados_{self.arq}.txt', 'a', encoding='UTF-8')
         cc = []
         vistos = np.zeros(self.v,dtype=int) #Vetor de zeros
         for vi in range(1,self.v +1):
@@ -166,7 +155,7 @@ class Graph_m: #grafo em matriz
                 cc.append(marcados[1])
         fim = np.asarray(cc)
         if p==1: 
-             arquivo_saida.write(f'\nNúmero de componentes conexas no grafo: {len(fim)}\nMaior componente conexa: {np.max(fim)}\nMenor componente conexa: {np.min(fim)}')
+             self.arquivo_saida.append(f'\nNúmero de componentes conexas no grafo: {len(fim)}\nMaior componente conexa: {np.max(fim)}\nMenor componente conexa: {np.min(fim)}')
         return cc    
 
     
